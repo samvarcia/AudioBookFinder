09fcf8d7c2f5d098de47ee36ed0e5c1dd9e9ce2e
Samuel Garcia <samfrontdev@gmail.com>
2021-02-10 18:51:49 -0500

    Initial setup ready ✅

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header'
+import '../assets/styles/App.scss'
+
+const App = () => (
+  <>
+    <Header />
+    <h1>Centro de control de Audiolibros</h1>
+  </>
+)
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BeekLogo from '../assets/img/BeekLogo.svg'
+
+const Header = () => {
+  return(
+    <header>
+      <img src={BeekLogo} alt="Beek Logo's"/>
+    </header>
+  )
+}
+
+export default Header;
